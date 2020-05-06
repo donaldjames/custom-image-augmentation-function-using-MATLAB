8407d737711e940ed6d87d28ad985bf45a6a6b80
Donald James <dony321@gmail.com>
2020-05-07 00:57:22 +0100

    Updated the demo test_run.m

--- test_run.m
+++ test_run.m
@@ -1,5 +1,15 @@
-input_text_file = "C:\Users\pc\Desktop\Image_processing_test\cars\test.txt";
-output_text_file = "updated_image_list.txt";
-destination_folder = "C:\Users\pc\Desktop\Image_processing_test\course_work\car_test_image";
-
-x = custom_augmentation_function(input_text_file, destination_folder, output_text_file);
+%%
+% The arguments to be passed are (update it and uncomment if you would like
+% to run the code using test_run.m else refer it for your ease of operation
+% input_text_file = ''; %% The .txt file with images names and label. Try
+%                       %% to provide full path for hussle free operation
+% destination_folder = ''; %% The destination folder where to save the 
+%                          %% images. Try to provide full path.  
+% output_text_file = '';%% The .txt file name to which you would like 
+%                       %% to save the images. Try to provide full path 
+%                       %% for hussle free operation
+% augmentation_list = ''; %% for eg [1 5 8] or [9] for all
+% grayscale = ''; %% optional argument. provide '1'  if you wan't grayscale
+%                 %% conversion
+%%
+x = custom_augmentation_function(input_text_file, destination_folder, output_text_file, augmentation_list, grayscale);
